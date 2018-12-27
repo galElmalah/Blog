0539d2f32d32d76c9f565333d0f077c87bc1b4d9
Gal Elmalah <gale@wix.com>
2018-12-27 11:14:44 +0200

    ImageContainer added css gradient loading animation

--- src/components/MainView/Blog/PostCard/PostCard.js
+++ src/components/MainView/Blog/PostCard/PostCard.js
@@ -1,36 +1,34 @@
-import * as React from "react";
-import { truncate } from "../utils";
-import * as s from "./postCard.scss";
-import { TagContainer } from "../../../Tag";
-import { Link } from "react-router-dom";
-import { Button } from "../../../Button/Button";
-import { Loader } from "../../../Loader/Loader";
+import * as React from 'react';
+import { truncate } from '../utils';
+import * as s from './postCard.scss';
+import { TagContainer } from '../../../Tag';
+import { Link } from 'react-router-dom';
+import { Button } from '../../../Button/Button';
+import { Loader } from '../../../Loader/Loader';
 
 const des = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
 
 export const PostCard = ({ title, imageUrl, tags = [], id, history }) => (
-  <div className={"post-card"}>
+  <div className={'post-card'}>
     <Link to={`/blog/${id}`}>
-      <section className={"info"}>
-        <div className={"image-container"}>
-          {imageUrl ? (
+      <section className={'info'}>
+        <div className={'image-container' + (!imageUrl ? ' loading' : '')}>
+          {imageUrl && (
             <img
-              className={"post-image"}
+              className={'post-image'}
               src={imageUrl}
               alt={`${title} related`}
             />
-          ) : (
-            <Loader />
           )}
         </div>
-        <div className={"text padding"}>
-          <h2 className={"post-title"}>{title}</h2>
-          <p className={"post-description"}>{truncate(des)}</p>
+        <div className={'text padding'}>
+          <h2 className={'post-title'}>{title}</h2>
+          <p className={'post-description'}>{truncate(des)}</p>
         </div>
       </section>
     </Link>
-    <section className={"padding"}>
-      <div className={"tags"}>
+    <section className={'padding'}>
+      <div className={'tags'}>
         <TagContainer>
           {({ addTagFilter }) => {
             return tags.map(tag => (
@@ -44,16 +42,16 @@ export const PostCard = ({ title, imageUrl, tags = [], id, history }) => (
       <span className="divider" />
 
       <Link
-        key={"share"}
+        key={'share'}
         to={`/blog/${id}`}
-        className={`btn ${!imageUrl ? " disabled" : ""}`}
+        className={`btn ${!imageUrl ? ' disabled' : ''}`}
       >
         <Button disabled={!imageUrl}>share</Button>
       </Link>
       <Link
-        key={"readMore"}
+        key={'readMore'}
         to={`/blog/${id}`}
-        className={`btn ${!imageUrl ? " disabled" : ""}`}
+        className={`btn ${!imageUrl ? ' disabled' : ''}`}
       >
         <Button disabled={!imageUrl}>read more</Button>
       </Link>
